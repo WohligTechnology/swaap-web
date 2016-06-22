f1afbc7cebe6fcc9463383e92eb87ee4bfbb5f42
TusharSachde <tushar@wohlig.com>
2016-06-22 12:28:46 +0530

    Color

---
+++ js/controllers.js
@@ -0,0 +1,76 @@
+angular.module('phonecatControllers', ['templateservicemod', 'navigationservice', 'ui.bootstrap','ngAnimate', 'ngSanitize', 'angular-flexslider'])
+
+.controller('HomeCtrl', function($scope, TemplateService, NavigationService, $timeout) {
+  //Used to name the .html file
+
+  console.log("Testing Consoles");
+
+  $scope.template = TemplateService.changecontent("home");
+  $scope.menutitle = NavigationService.makeactive("Home");
+  TemplateService.title = $scope.menutitle;
+  $scope.navigation = NavigationService.getnav();
+
+  $scope.mySlides = [
+    'http://flexslider.woothemes.com/images/kitchen_adventurer_cheesecake_brownie.jpg',
+    'http://flexslider.woothemes.com/images/kitchen_adventurer_lemon.jpg',
+    'http://flexslider.woothemes.com/images/kitchen_adventurer_donut.jpg',
+    'http://flexslider.woothemes.com/images/kitchen_adventurer_caramel.jpg'
+  ];
+
+  $scope.hows = [{
+      icon: 'img/i1.png',
+      title: 'Search for an expert',
+      desc: 'Search from a wide range of experts to answer all your questions'
+  }, {
+      icon: 'img/i2.png',
+      title: 'Compare experts',
+      desc: 'View expert profiles and shortlist them based on your criteria'
+  }, {
+      icon: 'img/i3.png',
+      title: 'Schedule your expert appointment',
+      desc: 'Schedule your phone consultation at a convenient time, Get a confirmation and only then pay for your call'
+  }, {
+      icon: 'img/i4.png',
+      title: 'Get on call with them',
+      desc: 'Get seamlessly connected with the expert. Have a great experience getting your query resolved'
+  }];
+
+
+
+
+})
+
+.controller('headerctrl', function($scope, TemplateService) {
+  $scope.template = TemplateService;
+  $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+    $(window).scrollTop(0);
+  });
+})
+
+.controller('languageCtrl', function($scope, TemplateService,$translate,$rootScope) {
+
+    $scope.changeLanguage = function() {
+      console.log("Language CLicked");
+
+      if(!$.jStorage.get("language")){
+        $translate.use("hi");
+        $.jStorage.set("language","hi");
+      }
+      else {
+        if($.jStorage.get("language") == "en")
+        {
+          $translate.use("hi");
+          $.jStorage.set("language","hi");
+        }
+        else {
+          $translate.use("en");
+          $.jStorage.set("language","en");
+        }
+      }
+    //  $rootScope.$apply();
+    };
+
+
+})
+
+;
---
+++ js/navigation.js
@@ -0,0 +1,49 @@
+var adminURL = "";
+if(isproduction)
+{
+  adminURL =  "http://www.wohlig.co.in/demo/index.php";
+}
+else {
+  adminURL = "http://localhost/demo/index.php";
+}
+
+var navigationservice = angular.module('navigationservice', [])
+
+.factory('NavigationService', function() {
+  var navigation = [{
+    name: "Home",
+    classis: "active",
+    anchor: "home",
+    subnav: [{
+      name: "Subnav1",
+      classis: "active",
+      anchor: "home"
+    },{
+      name: "Subnav2",
+      classis: "active",
+      anchor: "home"
+    }]
+  },{
+    name: "About",
+    classis: "active",
+    anchor: "about",
+    subnav: []
+  }];
+
+  return {
+    getnav: function() {
+      return navigation;
+    },
+    makeactive: function(menuname) {
+      for (var i = 0; i < navigation.length; i++) {
+        if (navigation[i].name == menuname) {
+          navigation[i].classis = "active";
+        } else {
+          navigation[i].classis = "";
+        }
+      }
+      return menuname;
+    },
+
+  };
+});
